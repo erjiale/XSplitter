19ff4f65c5881e7b8b6d3c56f8c83fede2fb27d1
newdevon <stvchan999@gmail.com>
2022-02-22 22:45:50 -0500

    XS-2 initialized virtual environment and input requirement

--- splitCheck.py
+++ splitCheck.py
@@ -1,3 +1,37 @@
+from operator import truediv
+import sys
+import keyboard
+
+def namesInput():
+    name_input = input("Enter a name: ")
+    # while True:
+        # name_input = input("Enter a name: ")
+    #     if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+    #             print('You Pressed A Key!')
+    #             break  # finishing the loop
+
+    
+
+def itemsInput():
+    itemList = []
+
+    while True:
+        item_name = input("What is the item? | Type 'DONE' to proceed: ")
+        if item_name == "DONE":
+            break
+        else:
+            item_price = input(f"What is {item_name} price? ")
+            itemList.append(f"{item_name} ${item_price}")
+        
+    print(itemList)    
+
 
 def splitCheck():
-    print("XSplit TIME!!!")
+
+    print("\033[96m" + "Welcome to Xplitter" + "\033[0m")
+    
+    title = input("Enter file title:")
+    print(title)
+    
+    # namesInput()
+    itemsInput()
